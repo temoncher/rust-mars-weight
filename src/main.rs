dab4da415192b42dbb88225a177ec0ded5bb0120
Artem Baranov <temoncher@yandex.ru>
2021-08-27 03:17:44 +0300

    add mars weight calulator implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Please, enter your weight (kg)");
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+    let mars_weight = calculate_weight_on_mars(weight);
+    println!("Your Mars weight {}kg", mars_weight);
+}
+
+fn calculate_weight_on_mars(earth_weight: f32) -> f32 {
+    (earth_weight / 9.81) * 3.711
 }
